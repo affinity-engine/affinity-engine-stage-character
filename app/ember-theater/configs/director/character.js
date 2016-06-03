0808354a0b528020d319cdfd3f22b3d55cb3d08b
null null null <null@ember.theater>
2016-06-03 08:32:37 -0700

    get acceptance test working

---
+++ app/ember-theater/configs/director/character.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-theater-director-character/ember-theater/configs/director/character';
--- tests/acceptance/director/directions/character-test.js
+++ tests/acceptance/director/directions/character-test.js
@@ -21,53 +21,51 @@ test('Ember Theater | Director | Directions | character', function(assert) {
     assert.equal(Ember.$(`${hook('expression_direction')} img`).attr('alt'), 'Bitsy', '`alt` is set by the fixture `caption`');
     assert.ok(Ember.$(`${hook('expression_direction')} img`).attr('src').match('theater/characters/bitsy/neutral.png'), 'it sets the `src` based on the associated fixture expression');
 
-    return step(100);
+    return step(75);
   }).then(() => {
     assert.equal(parseFloat($hook('character_direction').css('opacity')).toFixed(1), 0.2, '`transition` sets character css');
 
-    return step(100);
+    return step(75);
   }).then(() => {
     assert.equal(parseFloat($hook('character_direction').css('opacity')).toFixed(1), 0.5, '`transition`s can be chained');
 
-    return step();
+    return step(75);
   }).then(() => {
     assert.equal($hook('character_direction').length, 2, 'multiple instances of the same character can be rendered by setting `instance`');
 
-    return step(100);
+    return step(75);
   }).then(() => {
     assert.equal(parseFloat(Ember.$(`${hook('character_direction')}:nth(0)`).css('opacity')).toFixed(1), 0.5, 'instances respond independently to `transition`s: 1');
     assert.equal(parseFloat(Ember.$(`${hook('character_direction')}:nth(1)`).css('opacity')).toFixed(1), 0.6, 'instances respond independently to `transition`s: 2');
 
-    return step();
+    return step(75);
   }).then(() => {
     assert.equal($hook('character_direction').length, 3, 'characters with different fixtures can co-exist on screen');
 
-    return step();
+    return step(75);
   }).then(() => {
     assert.equal($hook('character_direction').length, 4, '`character` can be passed a fixture directly');
     assert.ok(Ember.$(`${hook('expression_direction')}:nth(3) img`).attr('src').match('theater/characters/emma/neutral.png'), 'the manually defined character defaultExpressionId is set properly');
 
-    return step(100);
+    return step(75);
   }).then(() => {
     assert.equal($hook('character_direction').length, 5, 'characters are rendered with `position`');
-    assert.equal(Ember.$(`${hook('character_direction')}:nth(4)`).css('left'), '640px', '`position` positions the character');
+    assert.equal(Ember.$(`${hook('character_direction')}:nth(4)`).css('left'), '50%', '`position` positions the character');
 
-    return step(200);
+    return step(150);
   }).then(() => {
     const $bitsy4 = Ember.$(`${hook('character_direction')}:nth(4)`);
 
-    assert.equal($bitsy4.css('left'), '256px', '`position` can accept multiple positions, Y');
+    assert.equal($bitsy4.css('left'), '20%', '`position` can accept multiple positions, Y');
+    assert.equal($bitsy4.css('bottom'), '-5%', '`position` can accept multiple positions, X');
 
-    // phantom return -5%, while Chrome return -38.375px
-    assert.ok(['-38.375px', '-5%'].indexOf($bitsy4.css('bottom')) > -1, '`position` can accept multiple positions, X');
-
-    return step();
+    return step(75);
   }).then(() => {
     assert.ok(Ember.$(`${hook('expression_direction')}:nth(5) img`).attr('src').match('theater/characters/bitsy/happy.png'), '`expression` can adjust the expression before rendering');
 
-    return step(100);
+    return step(75);
   }).then(() => {
     assert.ok(Ember.$(`${hook('expression_direction')}:nth(5) img`).attr('src').match('theater/characters/bitsy/sad.png'), '`expression` can adjust the expression after rendering');
-    assert.equal(Ember.$(`${hook('character_direction')}:nth(5)`).css('left'), '1024px', '`position` can be chained after expression');
+    assert.equal(Ember.$(`${hook('character_direction')}:nth(5)`).css('left'), '80%', '`position` can be chained after expression');
   });
 });
